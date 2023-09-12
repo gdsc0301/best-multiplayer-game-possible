0f476e1a42526b97a848f257011a1798a07d0647
Guilherme Carvalho <gdsc0301@gmail.com>
2023-09-12 19:12:45 -0300

    force test fail

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,19 +1,31 @@
-import { expect, test } from 'bun:test';
+import { describe, expect, test } from 'bun:test';
 import * as server from '../index';
 
-test('server status', async () => {
-  expect((await fetch(`http://localhost:${server.PORT}/`)).status).toBe(200)
-});
+describe('server', () => {
+  test('status', async () => {
+    expect((await fetch(`http://localhost:${server.PORT}/`)).status).toBe(200)
+  });
 
-let room_id = '';
-test('server login', async () => {
-  const response = await (await fetch(`http://localhost:${server.PORT}/login?player_email=bun-test`)).json();
-  room_id = response.data._ID;
-  expect(room_id === undefined).toBe(false);
-  expect(response.data.players['bun-test'].username).toBe('bun-test');
-});
+  let room_id = '';
+  test('login', async () => {
+    const response = await (await fetch(`http://localhost:${server.PORT}/login?player_email=bun-test`)).json();
+    room_id = response.data._ID;
+    expect(room_id === undefined).toBe(false);
+    expect(response.data.players['bun-test'].username).toBe('bun-test');
+  });
 
-test('server logout', async () => {
-  const response = await fetch(`http://localhost:${server.PORT}/logout?player_email=bun-test&room_id=${room_id}`);
-  expect(response.status).toBe(200);
+  test('logout', async () => {
+    const response = await fetch(`http://localhost:${server.PORT}/logout?player_email=bun-test&room_id=${room_id}`);
+    expect(response.status).toBe(200);
+  });
+
+  test.todo('force unknown path', async () => {
+    const response = await fetch(`http://localhost:${server.PORT}/unknown-path`);
+    expect(response.status).toBe(404);
+  });
+
+  test('force unknown method', async () => {
+    const response = await fetch(`http://localhost:${server.PORT}/`, { method: 'PUT' });
+    expect(response.status).toBe(405);
+  });
 });
